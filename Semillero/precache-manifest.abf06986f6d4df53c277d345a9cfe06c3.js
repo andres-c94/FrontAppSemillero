7722c054786f02c67dddf5a420798532e8407015
Andres C <69328357+andres-c94@users.noreply.github.com>
2022-03-30 14:59:45 -0500

    Add files via upload

---
+++ Semillero/precache-manifest.abf06986f6d4df53c277d345a9cfe06c3.js
@@ -0,0 +1,89 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d49b315be8e5cab255593b66f018d5d1",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a75cc8b87710b5169e56",
+    "url": "./static/css/2.150d169a.chunk.css"
+  },
+  {
+    "revision": "283342d6b825ff5c0464",
+    "url": "./static/css/2.150d169a.chunk.css.map"
+  },
+  {
+    "revision": "f4bca87fd0d19e61c27dc96299c75f8c",
+    "url": "./static/css/2.325dfaf0.chunk.css"
+  },
+  {
+    "revision": "5c34450ade8b4af574f37e3871e2db49",
+    "url": "./static/css/2.325dfaf0.chunk.css.map"
+  },
+  {
+    "revision": "b5ed933057eaf1919dd79d81248cdbce",
+    "url": "./static/css/main.1f9e400d.chunk.css"
+  },
+  {
+    "revision": "c37c52ac7cc2ae98e9c82c407ba6620e",
+    "url": "./static/css/main.1f9e400d.chunk.css.map"
+  },
+  {
+    "revision": "c37c52ac7cc23e3eae98e9c82c407ba6620e",
+    "url": "./static/css/main.c6cbe986.chunk.css"
+  },
+  {
+    "revision": "09fb5a28e0b737c45c3cbab20bf12a37",
+    "url": "./static/css/main.c6cbe986.chunk.css.map"
+  },
+
+  {
+    "revision": "a75cc8b87710b5169e56",
+    "url": "./static/js/2.6c7842ec.chunk.js"
+  },
+  {
+    "revision": "bc3d92b8a267d342eb7718b9a67f62b8",
+    "url": "./static/js/2.6c7842ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "283342d6b825ff5c0464",
+    "url": "./static/js/main.e912fcc6.chunk.js"
+  },
+  {
+    "revision": "c94aa43da009bd168dc1",
+    "url": "./static/js/runtime-main.cf4fd341.js"
+  },
+  {
+    "revision": "16e2c13011866713e391b142407d3750",
+    "url": "./static/media/COMPLETO_BLANCO.f2001b71.png"
+  },
+  {
+    "revision": "5cb5425303bbcd5e05bdd5ba086a62be",
+    "url": "./static/media/fondo_simple.69ebc683.jpeg"
+  },
+  {
+    "revision": "9363ea6ab443cd75aaef632cb1f26d7a",
+    "url": "./static/media/fondo.69ebc683.jpeg"
+  },
+  {
+    "revision": "d145db54caa95e741858e5bbf168d658",
+    "url": "./static/media/fondo.aafb3515.png"
+  },
+  {
+    "revision": "5444f4f2c1316c264cba5fc2a113d115",
+    "url": "./static/media/gafas.abf0b1d5.png"
+  },
+  {
+    "revision": "5a511e43ef8e70da1a9077e38c7bd645",
+    "url": "./static/media/imagen tajadas.71c6e389.jpg"
+  },
+  {
+    "revision": "37991b9e754954ea16eee9110b3c74cc",
+    "url": "./static/media/imagen_crearcuenta.419fcebc.png"
+  },
+  {
+    "revision": "de55ae52af85b8952e65d1b546992618",
+    "url": "./static/media/uc-social-share.e2374f81.jpg"
+  },
+
+  
+]);
